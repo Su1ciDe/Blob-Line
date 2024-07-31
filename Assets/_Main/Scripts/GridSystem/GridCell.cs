7c1d6154d0477f15ead4ac88aafd27c8eb34ae30
Murat Öz <murat.oz@fibergames.com.tr>
2024-07-31 10:52:30 +0300

    Added grid, hole

---
+++ Assets/_Main/Scripts/GridSystem/GridCell.cs
@@ -0,0 +1,17 @@
+using Interfaces;
+using UnityEngine;
+
+namespace GridSystem
+{
+	public class GridCell : MonoBehaviour
+	{
+		public Vector2Int Coordinates { get; private set; }
+		
+		public INode CurrentNode { get; set; }
+
+		public void Setup(int x, int y, Vector2 size)
+		{
+			Coordinates = new Vector2Int(x, y);
+		}
+	}
+}
